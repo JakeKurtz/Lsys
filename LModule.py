826bf638400df70312a0c272cc037899134b20fc
wanderlandeurasiers <wanderlandeurasiers@gmail.com>
2023-04-19 12:47:18 -0600

    LModule

--- LModule.py
+++ LModule.py
@@ -1,18 +1,52 @@
 import re
 
-def trim_brackets(string):
-    pass
+# ------------------------ Regular Expression Patterns ----------------------- #
+
+re_func_name = ' *[A-Za-z]{1} *(\\(|)'
+re_input_struct = '.*(:|).*(=|\\->).*(:|)'
+re_empty = '( *|^$)'
+
+re_valid_module = '( *[A-Za-z]{1} *\\( *([A-Za-z]+(?: *, *[A-Za-z]+)+|[A-Za-z]) *\\) *| *[A-Za-z]{1} *)|^$'
+re_valid_boolean = 'and|or|not|xor|<=|<|>|>=|!=|==|[<>!=]=|[<>]'
+re_valid_math_expression = '[ a-zA-Z0-9*+\\-*\\/%^]*'
+
+re_parameters = '\\((?:[^)(]+|\\((?:[^)(]+|\\([^)(]*\\))*\\))*\\)'
+re_module = '[A-Za-z]{1}(\\((?:[^)(]+|\\((?:[^)(]+|\\([^)(]*\\))*\\))*\\)|)'
 
 class LModule:
-    
+
     def __init__(self, input_str):
-        pass
+        self.__symbol = ""
+        self.__parameters = []
+        
+        match = re.match(re_valid_module, input_str)
+        if match:
+            self.__extract_symbol(input_str)
+            self.__extract_parameters(input_str)
+            self.__valid = True
+        else:
+            self.__symbol = input_str
+            self.__valid = False
 
-    def extract_symbol(self, input_str):
-        pass
-    def extract_parameters(self, input_str):
-        pass
+    def __extract_symbol(self, input_str):
+        match = re.search(re_func_name, input_str)
+        if match:
+            self.__symbol = re.sub('[() ]', '', match[0])
 
-    __symbol = ""
-    __parameters = []
-    __valid = False
+    def __extract_parameters(self, input_str):
+        match = re.search(re_parameters, input_str)
+        if match:
+            parameters_raw = re.sub('[()]', '', match[0])
+            parameters = parameters_raw.split(',')
+            self.__parameters.extend(parameters)
+            match = re.search(re_parameters, input_str)
+
+    @property
+    def valid(self):
+        return self.__valid
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def parameters(self):
+        return self.__parameters
